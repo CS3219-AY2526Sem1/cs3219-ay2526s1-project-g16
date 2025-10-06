5c3a38447eb3a3b229675ed197eff47c3ed3fb39
Axel Teo <teojunda@gmail.com>
2025-10-06 22:46:18 +0800

    Fix formatting with linter

--- backend/shared-middleware/dist/access-control.d.ts
+++ backend/shared-middleware/dist/access-control.d.ts
@@ -2,9 +2,13 @@ import type { Request, Response, NextFunction } from "express";
 import type { JwtPayload } from "jsonwebtoken";
 export * from "./express.js";
 export interface MyJwtPayload extends JwtPayload {
-    username: string;
-    email: string;
-    isAdmin: boolean;
+  username: string;
+  email: string;
+  isAdmin: boolean;
 }
-export declare function authenticateJWT(req: Request, res: Response, next: NextFunction): Promise<void>;
+export declare function authenticateJWT(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void>;
 //# sourceMappingURL=access-control.d.ts.map
--- backend/shared-middleware/dist/access-control.js
+++ backend/shared-middleware/dist/access-control.js
@@ -2,37 +2,36 @@ import jwt from "jsonwebtoken";
 const { TokenExpiredError } = jwt;
 export * from "./express.js";
 export async function authenticateJWT(req, res, next) {
-    const accessToken = req.cookies?.jwt_access_token;
-    if (!accessToken) {
-        res.status(401).json({ error: "Unauthorized: No access token provided" });
-        return;
+  const accessToken = req.cookies?.jwt_access_token;
+  if (!accessToken) {
+    res.status(401).json({ error: "Unauthorized: No access token provided" });
+    return;
+  }
+  try {
+    const accessSecret = process.env.ACCESS_JWT_SECRET;
+    if (!accessSecret) {
+      throw new Error("ACCESS_JWT_SECRET is not defined");
     }
-    try {
-        const accessSecret = process.env.ACCESS_JWT_SECRET;
-        if (!accessSecret) {
-            throw new Error("ACCESS_JWT_SECRET is not defined");
-        }
-        const decoded = jwt.verify(accessToken, accessSecret);
-        // Add user info to request object
-        req.user = {
-            id: decoded.sub,
-            username: decoded.username,
-            email: decoded.email,
-            isAdmin: decoded.isAdmin,
-        };
-        next(); // Proceed to next middleware or route handler
+    const decoded = jwt.verify(accessToken, accessSecret);
+    // Add user info to request object
+    req.user = {
+      id: decoded.sub,
+      username: decoded.username,
+      email: decoded.email,
+      isAdmin: decoded.isAdmin,
+    };
+    next(); // Proceed to next middleware or route handler
+  } catch (err) {
+    if (err instanceof TokenExpiredError) {
+      res.status(401).json({
+        error: "Unauthorized: Expired access token",
+      });
+      return;
     }
-    catch (err) {
-        if (err instanceof TokenExpiredError) {
-            res.status(401).json({
-                error: "Unauthorized: Expired access token",
-            });
-            return;
-        }
-        res.status(401).json({
-            error: "Unauthorized: Invalid token" + err,
-        });
-        return;
-    }
+    res.status(401).json({
+      error: "Unauthorized: Invalid token" + err,
+    });
+    return;
+  }
 }
 //# sourceMappingURL=access-control.js.map
