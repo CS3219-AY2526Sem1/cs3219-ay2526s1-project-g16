1a07c4498e9d3e9ddd4153abebae93b6d6686ac6
Axel Teo <teojunda@gmail.com>
2025-10-13 20:52:58 +0800

    Update default port number

--- backend/attempt-service/src/server.ts
+++ backend/attempt-service/src/server.ts
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Add attempt routes
 app.use("/attempt", attemptRoutes);
