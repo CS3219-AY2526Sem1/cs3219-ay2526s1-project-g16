c490c9153fce726c6362e8d75d7bceb61ecd1fc0
Axel Teo <teojunda@gmail.com>
2025-10-13 20:17:22 +0800

    Write model functions to get unique questions for a user, get all qns attempts for a user, create attempt for a user

--- backend/attempt-service/src/model/attempt.ts
+++ backend/attempt-service/src/model/attempt.ts
@@ -1,2 +1,54 @@
 import { prisma } from "./prisma-client.ts";
 import type { attempt } from "../generated/prisma/index.js";
+
+export async function addAttempt(
+  username: string,
+  matchUsername: string,
+  question: number,
+  code: string,
+): Promise<attempt> {
+  const newAttempt = await prisma.attempt.create({
+    data: {
+      username,
+      matchUsername,
+      question,
+      code,
+    },
+  });
+
+  return newAttempt;
+}
+
+export async function getAttemptsByUsername(
+  username: string,
+): Promise<attempt[]> {
+  const attempts = await prisma.attempt.findMany({
+    where: {
+      username,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return attempts;
+}
+
+export async function getUniqueQuestionsByUsername(
+  username: string,
+): Promise<number[]> {
+  const uniqueQuestions = await prisma.attempt.findMany({
+    where: {
+      username,
+    },
+    distinct: ["question"],
+    select: {
+      question: true,
+    },
+    orderBy: {
+      question: "asc",
+    },
+  });
+
+  return uniqueQuestions.map((entry) => entry.question);
+}
