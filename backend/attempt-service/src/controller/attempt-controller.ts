6ddb0eba1c5f266f93725044d87a91f617e0d6cf
Axel Teo <teojunda@gmail.com>
2025-10-13 20:44:19 +0800

    Apply lint fix

--- backend/attempt-service/src/controller/attempt-controller.ts
+++ backend/attempt-service/src/controller/attempt-controller.ts
@@ -1,10 +1,10 @@
 import type { Request, Response } from "express";
-import type { attempt } from '../generated/prisma/index.js';
+import type { attempt } from "../generated/prisma/index.js";
 import {
   addAttempt as _addAttempt,
   getAttemptsByUserId as _getAttemptsByUserId,
   getUniqueQuestionsByUserId as _getUniqueQuestionsByUserId,
-} from '../model/attempt.ts';
+} from "../model/attempt.ts";
 
 export async function helloWorld(req: Request, res: Response): Promise<void> {
   try {
@@ -22,41 +22,39 @@ export async function addAttempt(req: Request, res: Response): Promise<void> {
     const { userId, matchUsername, question, code } = req.body;
 
     if (req.user?.id != userId) {
-      res.status(401).json({ error: 'Unauthorized to add this attempt' });
+      res.status(401).json({ error: "Unauthorized to add this attempt" });
       return;
     }
 
     if (
-      typeof userId !== 'string' ||
-      typeof matchUsername !== 'string' ||
-      typeof question !== 'number' ||
-      typeof code !== 'string'
+      typeof userId !== "string" ||
+      typeof matchUsername !== "string" ||
+      typeof question !== "number" ||
+      typeof code !== "string"
     ) {
-      res.status(400).json({ error: 'Invalid input' });
+      res.status(400).json({ error: "Invalid input" });
       return;
     }
 
-    const newAttempt: attempt = await _addAttempt(
-      userId,
-      matchUsername,
-      question,
-      code,
-    );
+    await _addAttempt(userId, matchUsername, question, code);
 
-    res.status(201).json({ message: 'Attempt created' });
+    res.status(201).json({ message: "Attempt created" });
     return;
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error: " + error });
     return;
   }
 }
 
-export async function getAttemptsByUserId(req: Request, res: Response): Promise<void> {
+export async function getAttemptsByUserId(
+  req: Request,
+  res: Response,
+): Promise<void> {
   try {
     const userId = req.params.userId;
 
-    if (typeof userId !== 'string') {
-      res.status(400).json({ error: 'Invalid username' });
+    if (typeof userId !== "string") {
+      res.status(400).json({ error: "Invalid username" });
       return;
     }
 
@@ -64,20 +62,27 @@ export async function getAttemptsByUserId(req: Request, res: Response): Promise<
     const page = Number(req.query.page) || 0;
     const pageSize = Number(req.query.pageSize) || 10;
 
-    const attempts: attempt[] = await _getAttemptsByUserId(userId, page, pageSize);
+    const attempts: attempt[] = await _getAttemptsByUserId(
+      userId,
+      page,
+      pageSize,
+    );
 
     res.status(200).json(attempts);
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" + error });
   }
 }
 
-export async function getUniqueQuestionsByUserId(req: Request, res: Response): Promise<void> {
+export async function getUniqueQuestionsByUserId(
+  req: Request,
+  res: Response,
+): Promise<void> {
   try {
     const username = req.params.userId;
 
-    if (typeof username !== 'string') {
-      res.status(400).json({ error: 'Invalid username' });
+    if (typeof username !== "string") {
+      res.status(400).json({ error: "Invalid username" });
       return;
     }
 
@@ -85,7 +90,7 @@ export async function getUniqueQuestionsByUserId(req: Request, res: Response): P
 
     res.status(200).json(questions);
   } catch (error) {
-    console.error('Error fetching unique questions:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    console.error("Error fetching unique questions:", error);
+    res.status(500).json({ error: "Internal server error" });
   }
 }
