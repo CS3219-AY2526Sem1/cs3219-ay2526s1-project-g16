a9805982e0393685127ad8ec15b705020af3e51b
Axel Teo <teojunda@gmail.com>
2025-10-13 20:46:28 +0800

    Add routes for attempt history

--- backend/attempt-service/src/routes/attempt-routes.ts
+++ backend/attempt-service/src/routes/attempt-routes.ts
@@ -1,9 +1,15 @@
 import express from "express";
-import { helloWorld } from "../controller/attempt-controller.ts";
+import { helloWorld, addAttempt, getAttemptsByUserId, getUniqueQuestionsByUserId } from "../controller/attempt-controller.ts";
 import { authenticateJWT } from "shared-middleware";
 
 const router = express.Router();
 
 router.get("/", authenticateJWT, helloWorld);
+
+router.post("/", authenticateJWT, addAttempt);
+
+router.get('/:userId', authenticateJWT, getAttemptsByUserId);
+
+router.get('/unique-qns/:userId', getUniqueQuestionsByUserId);
 
 export default router;
