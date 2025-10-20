e16a202239e39790c3384816d245d7156bb26cd3
jonasongg <jonasong2@gmail.com>
2025-10-20 21:11:31 +0800

    add find match and cancel match routes

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -5,3 +5,7 @@ export const USER_SERVICE_URL = import.meta.env.DEV
 export const QN_SERVICE_URL = import.meta.env.DEV
   ? "http://localhost:3001/api"
   : "";
+
+export const MATCH_SERVICE_URL = import.meta.env.DEV
+  ? "http://localhost:3002"
+  : "";
--- frontend/src/routes/_authenticated/index.tsx
+++ frontend/src/routes/_authenticated/index.tsx
@@ -1,12 +1,13 @@
 import { MultiMatchMeSelect } from "@/components/matchMeSelect";
-import { QN_SERVICE_URL } from "@/constants";
+import { Button } from "@/components/ui/button";
+import { MATCH_SERVICE_URL, QN_SERVICE_URL } from "@/constants";
 import { authFetch } from "@/lib/utils";
 import {
   questionDifficulties,
   type ListLanguagesResponse,
   type ListTopicsResponse,
 } from "@/types";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { Loader2 } from "lucide-react";
 import { useState } from "react";
@@ -20,6 +21,10 @@ function Home() {
   const [questionTopics, setQuestionTopics] = useState<string[]>([""]);
   const [difficultyLevels, setDifficultyLevels] = useState<string[]>([""]);
   const [languages, setLanguages] = useState<string[]>([""]);
+  const [isFindingMatch, setIsFindingMatch] = useState(false);
+
+  const { auth } = Route.useRouteContext();
+  const { user } = auth;
 
   const topicsQuery = useQuery<ListTopicsResponse>({
     queryKey: ["questionTopics"],
@@ -43,6 +48,47 @@ function Home() {
     },
   });
 
+  const findMatchMutation = useMutation({
+    mutationFn: async () => {
+      const res = await authFetch(`${MATCH_SERVICE_URL}/match/request`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          userId: user?.id,
+          topicIn: questionTopics.filter((x) => !!x),
+          difficultyIn: difficultyLevels.filter((x) => !!x),
+          languageIn: languages.filter((x) => !!x),
+        }),
+      });
+      if (!res.ok) {
+        throw new Error("Match response was not ok");
+      }
+      return res.json();
+    },
+    onSuccess: () => {
+      setIsFindingMatch(true);
+    },
+  });
+
+  const cancelMatchMutation = useMutation({
+    mutationFn: async () => {
+      const res = await authFetch(`${MATCH_SERVICE_URL}/match/cancel`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          userId: user?.id,
+        }),
+      });
+      if (!res.ok) {
+        throw new Error("Cancel match response was not ok");
+      }
+      return res.json();
+    },
+    onSuccess: () => {
+      setIsFindingMatch(false);
+    },
+  });
+
   // at most toast once
   const queryIsError = topicsQuery.isError || langsQuery.isError;
   if (queryIsError) {
@@ -53,60 +99,96 @@ function Home() {
 
   return (
     <main className="mt-24 flex w-full flex-col items-center justify-center gap-8">
-      <h1 className="text-7xl font-semibold">Match Me</h1>
+      {isFindingMatch ? (
+        <h1 className="after:animate-ellipsis flex text-7xl font-semibold text-pink-800/70 after:block after:w-0">
+          Searching
+        </h1>
+      ) : (
+        <h1 className="text-7xl font-semibold">Match Me</h1>
+      )}
       {topicsQuery.isPending || langsQuery.isPending ? (
         <div className="mt-6 flex items-center gap-4 text-2xl text-neutral-700">
           <Loader2 className="animate-spin" />
           Loading...
         </div>
       ) : (
-        <div className="mr-4 grid grid-cols-2 gap-x-2 gap-y-2 text-3xl">
-          <span className="text-right">I want to do</span>
-          <MultiMatchMeSelect
-            placeholder="Question Topics"
-            choices={
-              topicsQuery.data?.topics?.map(({ name }) => ({
-                value: name,
-                label: name,
-              })) ?? []
-            }
-            triggerClassName="text-blue-500"
-            itemClassName="text-blue-900/80 focus:text-blue-500 data-[state=checked]:text-blue-500 [&_svg]:!text-blue-800/50"
-            items={questionTopics}
-            setItems={setQuestionTopics}
-            disabled={queryIsError}
-          />
+        <>
+          <div className="mr-4 grid grid-cols-2 gap-x-2 gap-y-2 text-3xl">
+            <span className="text-right">I want to do</span>
+            <MultiMatchMeSelect
+              placeholder="Question Topics"
+              choices={
+                topicsQuery.data?.topics?.map(({ name }) => ({
+                  value: name,
+                  label: name,
+                })) ?? []
+              }
+              triggerClassName="text-blue-500"
+              itemClassName="text-blue-900/80 focus:text-blue-500 data-[state=checked]:text-blue-500 [&_svg]:!text-blue-800/50"
+              items={questionTopics}
+              setItems={setQuestionTopics}
+              disabled={queryIsError}
+            />
 
-          <span className="text-right">at difficulty level</span>
-          <MultiMatchMeSelect
-            placeholder="Difficulty Level"
-            choices={questionDifficulties.map((level) => ({
-              value: level,
-              label: level,
-            }))}
-            triggerClassName="text-orange-500"
-            itemClassName="text-orange-900/80 focus:text-orange-500 data-[state=checked]:text-orange-500 [&_svg]:!text-orange-800/50"
-            items={difficultyLevels}
-            setItems={setDifficultyLevels}
-            disabled={queryIsError}
-          />
+            <span className="text-right">at difficulty level</span>
+            <MultiMatchMeSelect
+              placeholder="Difficulty Level"
+              choices={questionDifficulties.map((level) => ({
+                value: level,
+                label: level,
+              }))}
+              triggerClassName="text-orange-500"
+              itemClassName="text-orange-900/80 focus:text-orange-500 data-[state=checked]:text-orange-500 [&_svg]:!text-orange-800/50"
+              items={difficultyLevels}
+              setItems={setDifficultyLevels}
+              disabled={queryIsError}
+            />
 
-          <span className="text-right">using</span>
-          <MultiMatchMeSelect
-            placeholder="Language"
-            choices={
-              langsQuery.data?.languages?.map(({ name }) => ({
-                value: name,
-                label: name,
-              })) ?? []
-            }
-            triggerClassName="text-red-500"
-            itemClassName="text-red-900/80 focus:text-red-500 data-[state=checked]:text-red-500 [&_svg]:!text-red-800/50"
-            items={languages}
-            setItems={setLanguages}
-            disabled={queryIsError}
-          />
-        </div>
+            <span className="text-right">using</span>
+            <MultiMatchMeSelect
+              placeholder="Language"
+              choices={
+                langsQuery.data?.languages?.map(({ name }) => ({
+                  value: name,
+                  label: name,
+                })) ?? []
+              }
+              triggerClassName="text-red-500"
+              itemClassName="text-red-900/80 focus:text-red-500 data-[state=checked]:text-red-500 [&_svg]:!text-red-800/50"
+              items={languages}
+              setItems={setLanguages}
+              disabled={queryIsError}
+            />
+          </div>
+
+          {isFindingMatch ? (
+            <Button
+              size="lg"
+              className="bg-red-500 hover:bg-red-600"
+              onClick={() => cancelMatchMutation.mutate()}
+            >
+              Cancel Matching
+            </Button>
+          ) : (
+            <Button
+              size="lg"
+              className="bg-teal-600 hover:bg-teal-700"
+              onClick={() => findMatchMutation.mutate()}
+              disabled={
+                questionTopics.filter((x) => !!x).length === 0 ||
+                difficultyLevels.filter((x) => !!x).length === 0 ||
+                languages.filter((x) => !!x).length === 0 ||
+                findMatchMutation.isPending ||
+                queryIsError
+              }
+            >
+              {findMatchMutation.isPending && (
+                <Loader2 className="mr-2 h-6 w-6 animate-spin" />
+              )}
+              Match Me!
+            </Button>
+          )}
+        </>
       )}
     </main>
   );
