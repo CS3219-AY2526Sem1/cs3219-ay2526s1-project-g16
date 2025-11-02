010ab977d539d34a2150fd97c6b2b6c0f9ec77d2
jonasongg <jonasong2@gmail.com>
2025-11-02 17:45:45 +0800

    fix api urls again again

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,9 +1,9 @@
 export const USER_SERVICE_URL = import.meta.env.DEV
-  ? process.env.VITE_DEV_USER_SERVICE_URL
-  : `${import.meta.env.VITE_APIGATEWAY_URL}/user`;
+  ? import.meta.env.VITE_DEV_USER_SERVICE_URL
+  : `${process.env.VITE_APIGATEWAY_URL}/user`;
 export const QN_SERVICE_URL = import.meta.env.DEV
-  ? process.env.VITE_DEV_QN_SERVICE_URL
-  : `${import.meta.env.VITE_APIGATEWAY_URL}/question`;
+  ? import.meta.env.VITE_DEV_QN_SERVICE_URL
+  : `${process.env.VITE_APIGATEWAY_URL}/question`;
 export const MATCH_SERVICE_URL = import.meta.env.DEV
-  ? process.env.VITE_DEV_MATCH_SERVICE_URL
-  : `${import.meta.env.VITE_APIGATEWAY_URL}/match`;
+  ? import.meta.env.VITE_DEV_MATCH_SERVICE_URL
+  : `${process.env.VITE_APIGATEWAY_URL}/match`;
