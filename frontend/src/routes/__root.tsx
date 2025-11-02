964575a9d5d5b7aa564877ddc771e19bb09f721f
jonasongg <jonasong2@gmail.com>
2025-11-02 15:20:01 +0800

    remove devtools

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,8 +1,6 @@
 import { Toaster } from "@/components/ui/sonner";
 import type { AuthContextType } from "@/hooks/useAuth";
-import { TanstackDevtools } from "@tanstack/react-devtools";
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
 type RouterContext = {
   auth: AuthContextType;
@@ -13,17 +11,6 @@ export const Route = createRootRouteWithContext<RouterContext>()({
     <>
       <Outlet />
       <Toaster position="bottom-center" />
-      <TanstackDevtools
-        config={{
-          position: "bottom-left",
-        }}
-        plugins={[
-          {
-            name: "Tanstack Router",
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-        ]}
-      />
     </>
   ),
 });
