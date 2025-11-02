6f5e3abeabf154a26267b5334dcc2c66ac389d82
jonasongg <jonasong2@gmail.com>
2025-11-02 16:17:33 +0800

    fix api URLs

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,8 +1,9 @@
 export const USER_SERVICE_URL = import.meta.env.DEV
-  ? "http://localhost:3000"
-  : "";
+  ? import.meta.env.VITE_DEV_USER_SERVICE_URL
+  : `${import.meta.env.VITE_APIGATEWAY_URL}/user`;
 export const QN_SERVICE_URL = import.meta.env.DEV
-  ? "http://localhost:3001/api"
-  : "";
-
-export const MATCH_SERVICE_URL = `${import.meta.env.VITE_VM_URL}:3001`;
+  ? import.meta.env.VITE_DEV_QN_SERVICE_URL
+  : `${import.meta.env.VITE_APIGATEWAY_URL}/question`;
+export const MATCH_SERVICE_URL = import.meta.env.DEV
+  ? import.meta.env.VITE_DEV_MATCH_SERVICE_URL
+  : `${import.meta.env.VITE_APIGATEWAY_URL}/match`;
