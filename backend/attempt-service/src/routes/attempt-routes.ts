f7cc270ba611a1c576f86da7cfffe2559ca1ebc0
Axel Teo <teojunda@gmail.com>
2025-11-13 01:06:11 +0800

    Add route to get attempt by id

--- backend/attempt-service/src/routes/attempt-routes.ts
+++ backend/attempt-service/src/routes/attempt-routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { addAttempt, getAttemptsByUserId, getUniqueQuestionsByUserId } from "../controller/attempt-controller.ts";
+import { addAttempt, getAttemptsByUserId, getUniqueQuestionsByUserId, getAttempt } from "../controller/attempt-controller.ts";
 
 const router = express.Router();
 
@@ -7,7 +7,7 @@ router.post("/", addAttempt);
 
 router.get('/user/:id', getAttemptsByUserId);
 
-router.get('/:id', getAttemptsByUserId);
+router.get('/:id', getAttempt);
 
 router.get('/unique-qns/:id', getUniqueQuestionsByUserId);
 
